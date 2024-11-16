0935abc70f78603e3ec015147d68a0a649de9948
Abdwarwar <abdelsalam.warwar@hotmail.com>
2024-11-16 20:06:37 +0300

    Update editableTable.js

--- editableTable.js
+++ editableTable.js
@@ -1,24 +1,9 @@
-customElements.define('com-custom-editabletable', class EditableTable extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({ mode: 'open' });
-        this.shadowRoot.innerHTML = `
-            <style>
-                table { width: 100%; border-collapse: collapse; }
-                td, th { border: 1px solid #ccc; padding: 8px; }
-                input { width: 100%; }
-            </style>
-            <table>
-                <thead>
-                    <tr><th>Name</th><th>Value</th></tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><input type="text" value="Sample Name"></td>
-                        <td><input type="number" value="100"></td>
-                    </tr>
-                </tbody>
-            </table>
-        `;
+sap.ui.define([], function() {
+  return {
+    init: function() {
+      var div = document.createElement("div");
+      div.innerHTML = "Hello, World!";
+      this.getContainer().appendChild(div);
     }
+  };
 });
